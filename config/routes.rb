1457849e8c37e15103122a8e0e2af2c218fd3bcf
Devan Beitel <devan.beitel@gmail.com>
2014-07-11 10:50:37 -0500

    Style posts views. Add new/create/edit/update controller actions and routes.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Bloccit::Application.routes.draw do
 
   get 'about' => 'welcome#about'
 
-  root to: 'welcome#index'
+  root to: 'welcome#index' # => root({to: 'welcome#index'})
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
