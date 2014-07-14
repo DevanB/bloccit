b00c9a65c7ad137ffea423b824be8cac4b5ac659
Devan Beitel <devan.beitel@gmail.com>
2014-07-14 17:27:48 -0500

    Added comment creation.jJ

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,23 @@
 class CommentsController < ApplicationController
-  def show
+  def create
+    @topic = Topic.find(params[:topic_id])
+    @post = @topic.posts.find(params[:post_id])
+    
+    @comment = current_user.comments.build(comment_params)
+    @comment.post = @post
+
+    if @comment.save
+      flash[:notice] = "Comment was created."
+      redirect_to [@topic, @post]
+    else
+      flash[:error] = "There was an error saving the comment. Please try again."
+      render :new
+    end
   end
 
-  def create
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
   end
 end
