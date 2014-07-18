f28d1e722741aa7b0d7d59ab8ff09ffbd182a5ef
Devan Beitel <devan.beitel@gmail.com>
2014-07-18 14:07:21 -0500

    Added tests for votes controller, vote model and post model.

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 describe Vote do
+  include TestFactories
+
   describe "validations" do
     describe "value validation" do
       it "only allows -1 or 1 as values" do
@@ -10,4 +12,13 @@ describe Vote do
       end
     end
   end
+
+  describe 'after_save' do
+    it "calls `Post#update_rank` after save" do
+      post = post_without_user
+      vote = Vote.new(value: 1, post: post)
+      expect(post).to receive(:update_rank)
+      vote.save
+    end
+  end  
 end
