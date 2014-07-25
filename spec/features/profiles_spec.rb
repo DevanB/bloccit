76ec79eadf044bdb3f7472a3668769068fa6d843
Devan Beitel <devan.beitel@gmail.com>
2014-07-24 22:42:02 -0500

    Refactor comment creation. Add Warden helpers to login as admin.

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -2,18 +2,31 @@ require 'rails_helper'
 
 describe "Visiting profiles" do
   include TestFactories
+  include Warden::Test::Helpers
 
   before do
+    Warden.test_mode!
+    @admin = authenticated_user(role: 'admin')
     @user = authenticated_user
     @topic = Topic.create(description: "A topic")
     @post = post_without_user(user: @user, topic: @topic)
-    @comment = Comment.new(user: @user, body: "A Comment")
-    allow(@comment).to receive(:send_favorite_emails)
-    @comment.save
+    @comment = comment_without_email(user: @user, post: @post)
   end
 
   describe "not signed in" do
     it "shows profile" do
+      visit user_path(@user)
+      expect(current_path).to eq(user_path(@user))
+
+      expect(page).to have_content(@user.name)
+      expect(page).to have_content(@post.title)
+      expect(page).to have_content(@comment.body)
+    end
+  end
+
+  describe "signed in" do
+    it "shows profile" do
+      login_as(@admin, scope: :user)
       visit user_path(@user)
       expect(current_path).to eq(user_path(@user))
 
